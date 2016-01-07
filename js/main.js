5878554ff9e0fbb622aa651fb1d8d96d2156dae3
kushal tirumala <kushaltirumala99@gmail.com>
2016-01-07 00:48:43 -0800

    parse credentials

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$('#toggle-login').click(function(){
+  $('#login').toggle();
+});
+
+
+function login() {
+alert(document.getElementById('username').value);
+
+
+
+// Parse.initialize("CSkLcrK8SabIjCYg4fbwlAgWpXb7jkXn1hqq3hta", "9UehYmjHUGLgDximR5xMsmMZJMJ67klJZAgBcb0q");
+
+
+
+// var username = document.getElementById('usernmae').value;
+// var password = document.getElementById('password').value;
+
+//     alert(username);
+//     alert(password);
+
+// Parse.User.logIn(username, password, {
+//         success: function(user) {
+//             console.log(user);
+//             return true;
+//         },
+//         error: function(user, error) {
+//             console.log(user + error);
+//             return false;
+//         }
+//     });
+
+// var botao = document.querySelector('login');
+// botao.onclick = fazerLogin;
+}
