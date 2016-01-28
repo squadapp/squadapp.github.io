8f53518be88df3208a72fc41531cd9cea997ad5a
kushal tirumala <kushaltirumala99@gmail.com>
2016-01-27 21:12:52 -0800

    login works

--- js/main.js
+++ js/main.js
@@ -2,41 +2,80 @@ $('#toggle-login').click(function(){
   $('#login').toggle();
 });
 
+var loaded = false;
 
+//Parse.initialize(UNIQUEID, JSKEY);
 
-var UNIQUEID, JSKEY;
+function init() {
 
+    var UNIQUEID, JSKEY;
 
-UNIQUEID =  ID;
-JSKEY = js;
+    UNIQUEID =  ID;
+    JSKEY = js;
+    Parse.initialize(UNIQUEID, JSKEY);
+    loaded = true;
+    alert('INITIALIZING...');
+}
 
+function signup() {
+    var newusername = document.getElementById('newusername').value;
+    var newpassword = document.getElementById('newpassword').value;
 
+    alert(newusername + " " + newpassword);
 
-
-
-function login() {
-alert(document.getElementById('username').value);
-
-
-
-Parse.initialize(UNIQUEID, JSKEY);
-
-
-
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-
-alert(username);
-alert(password);
-
-Parse.User.logIn(username, password, {
+    Parse.user.signup(newusername, newpassword {
         success: function(user) {
             console.log(user);
             return true;
         },
         error: function(user, error) {
-            console.log(user + error);
+            console.log(user + " " + error);
+            return false;
+        }
+    })
+}
+
+
+function getClubs(user) {
+
+    var temp = user.get("officerClubs");
+    
+    if(temp == null) {
+        console.log("you're not an officer LOOOOL");
+    } else {
+        for(var i = 0; i < temp.length; i++) {
+            console.log("club " + i + " " + temp[i].name);
+        }
+    }
+}
+
+function errorMsg(user, error) {
+    alert(user + error);
+}
+function login() {
+
+if(!loaded) {
+    //alert("first time");
+    init();
+}
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+//console.log("username: " + username + " password: " + password);
+Parse.User.logIn(username, password, {
+        success: function(user) {
+            getClubs(user);
+            return true;
+        },
+        error: function(user, error) {
+            // errorMsg();
+            console.log(error);
             return false;
         }
     });
+
+
+//alert(password);
 }
+
+
+
